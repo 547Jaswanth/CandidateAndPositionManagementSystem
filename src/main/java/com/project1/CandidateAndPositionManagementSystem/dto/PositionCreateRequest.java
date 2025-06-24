bcd8b304f60b4effb42261ac1b0e45913d60369b
K Jaswanth <jaswanth.keeramanda@gmail.com>
2025-06-24 11:33:03 +0530

    Dummy Comit

--- src/main/java/com/project1/CandidateAndPositionManagementSystem/dto/PositionCreateRequest.java
+++ src/main/java/com/project1/CandidateAndPositionManagementSystem/dto/PositionCreateRequest.java
@@ -3,7 +3,6 @@ package com.project1.CandidateAndPositionManagementSystem.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-
 @Data
 public class PositionCreateRequest {
     @NotBlank(message = "position must not be blank..")
